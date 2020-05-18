b7637b2de57c75b30aaf8c0fe57fce48614ea5e5
Erick-D-S <65356418+Erick-D-S@users.noreply.github.com>
2020-05-18 16:51:52 -0500

    adding functionality to main and welcome screen

--- main.c
+++ main.c
@@ -4,14 +4,25 @@
 int main()
 {
 	howManyCustomers = 0;
+	int decision = 0; 
 	struct customer first; /*should this be an array?*/
 	while(1)
 	{
+		
 		welcomeScreen();
-		createUser(&first);
-		showBalance();
-		depositMoney();
-		withdrawMoney();
+		scanf("%d", &decision);
+		
+		if(decision == 1)
+		{
+			createUser(&first);
+		}
+		
+		else if(decision == 2)
+		{
+			showBalance();
+			depositMoney();
+			withdrawMoney();
+		}
 	}
 	return 0;
 }

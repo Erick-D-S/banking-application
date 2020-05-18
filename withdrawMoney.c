e9d4228e3f8d84a610823fe38bc841e7b3de9070
kthomas137 <65356425+kthomas137@users.noreply.github.com>
2020-05-18 15:52:12 -0500

    Update withdrawMoney.c

--- withdrawMoney.c
+++ withdrawMoney.c
@@ -11,25 +11,25 @@ void withdrawMoney()
     scanf("%d", &no);
     for (b = 0; b < i; b++)
     {
-        if (s[b].no == no)
+        if (customer[b].no == no)
             m = b;
     }
-    if (s[m].no == no)
+    if (customer[m].no == no)
     {
-        printf("\n Account Number : %d", s[m].no);
-        printf("\n Name : %s", s[m].name);
-        printf("\n Deposit : %f", s[m].dep);
+        printf("\n Account Number : %d", customer[m].no);
+        printf("\n Name : %s", customer[m].name);
+        printf("\n Deposit : %f", customer[m].dep);
         printf("\n How Much Withdraw Now:");
         scanf("%f", &aa);
-        if (s[m].dep < aa + 500)
+        if (customer[m].dep < aa + 500)
         {
             printf("\nCANNOT WITHDRAW YOUR ACCOUNT HAS MINIMUM BALANCE");
             getch();
         }
         else
         {
-            s[m].dep -= aa;
-            printf("\nThe Balance Amount is:%f", s[m].dep);
+            customer[m].dep -= aa;
+            printf("\nThe Balance Amount is:%f", customer[m].dep);
         }
     }
     else

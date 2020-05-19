bd24d83ad6b3548159e7ead438680507c2512b41
kthomas137 <65356425+kthomas137@users.noreply.github.com>
2020-05-19 18:49:32 -0500

    Update_working_1

--- showBalance.c
+++ showBalance.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include "functions.h"
 
-void showBalance()
+void showBalance(struct customer customerName)
 {
+	printf("\n Your balance is $%d \n", customerName.balance);
 	return;
 }

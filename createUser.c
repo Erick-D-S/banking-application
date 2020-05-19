e0fd3ff48945cca10d0a8264b940cbb4af53a216
kthomas137 <65356425+kthomas137@users.noreply.github.com>
2020-05-19 18:48:26 -0500

    Update_working_1

--- createUser.c
+++ createUser.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include "functions.h"
 
-void createUser(struct customer * new)
+void createUser(struct customer * customerName)
 {
+
 	howManyCustomers++;
+	customerName->firstName[0] = 'A';
+	customerName->lastName[0] = 'T';
+	customerName->id = howManyCustomers;
+	customerName->balance = 0;
+	printf(" Welcome to Bank \n");
+	printf(" Customer Name %s %s \n", customerName->firstName, 
+										customerName->lastName);
+	printf(" Your Bank ID is %d \n", customerName->id);
+	
+	
 	return;
 }

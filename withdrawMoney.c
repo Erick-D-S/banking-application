d5658755404df4e0133501b420eede2a90371c4f
kthomas137 <65356425+kthomas137@users.noreply.github.com>
2020-05-19 18:52:08 -0500

    Update_working_money

--- withdrawMoney.c
+++ withdrawMoney.c
@@ -1,6 +1,18 @@
+#include <stdio.h>
 #include "functions.h"
 
-void withdrawMoney()
+void withdrawMoney(struct customer *customerName, int amount)
 {
-   return;
+   
+   if (amount > (customerName->balance))
+	{
+		printf("\n Insufficient Balance \n");
+	}
+	else
+	{
+		customerName->balance = customerName->balance - amount;
+		printf("\nPlease Collect your cash\n");
+		printf("\nYour current balance after withdrawal is %d\n", customerName->balance);
+	}
+	return;
 }

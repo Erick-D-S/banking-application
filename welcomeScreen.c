99b96ead3a705fc14306598de32d74b9f915a3d4
Erick-D-S <65356418+Erick-D-S@users.noreply.github.com>
2020-05-20 15:31:17 -0500

    Fixed program flow

--- welcomeScreen.c
+++ welcomeScreen.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "functions.h"
 
 void welcomeScreen()
 {
@@ -6,5 +7,16 @@ void welcomeScreen()
 	printf("Choose one of the following options:\n");
 	printf("1. Create new customer\n");
 	printf("2. Log in\n");
+	return;
+}
+
+void logInScreen()
+{
+	printf("Welcome to the bank\n");
+	printf("Choose one of the following options:\n");
+	printf("1. Deposit money\n");
+	printf("2. Withdraw money\n");
+	printf("3. Show balance\n");
+	printf("4. Log out\n");
 	return;
 }

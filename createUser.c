c29df5bc69b7eadc4d95a9497b9435db3a479eb0
Erick-D-S <65356418+Erick-D-S@users.noreply.github.com>
2020-05-20 22:59:33 -0500

    complete overhaul to take advantage of dynamic memory allocation

--- createUser.c
+++ createUser.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "functions.h"
 
-void createUser(struct customer * customerName)
+
+void createUser(struct customer ** headNode, char first[], char last[])
 {
 
 	howManyCustomers++;
-	customerName->firstName[0] = 'A';
-	customerName->lastName[0] = 'T';
-	customerName->id = howManyCustomers;
-	customerName->balance = 0;
-	printf(" Welcome to Bank \n");
-	printf(" Customer Name %s %s \n", customerName->firstName, 
-										customerName->lastName);
-	printf(" Your Bank ID is %d \n", customerName->id);
-	
-	
+	struct customer * new_node = (struct customer*)malloc(sizeof(struct customer));
+	new_node->next = *headNode;
+	strncpy(new_node->firstName, first, 10);
+	strncpy(new_node->lastName, last, 10);
+	new_node->id = howManyCustomers;
+	new_node->balance = 0;
+	/*printf(" Customer Name %s %s \n", new_node->firstName,
+										new_node->lastName);
+	printf(" Your Bank ID is %d \n", new_node->id); */
+
+	*headNode = new_node;
+	return;
+}
+
+void findUser(struct customer ** list, int * id)
+{
+	while(*list != NULL)
+	{
+		if((*list)->id == *id)
+			break;
+		else
+			*list = (*list)->next;
+
+	}
+	if(*list == NULL)
+	{
+		printf("Invalid id \n");
+		*id = 0;
+	}
+
 	return;
 }

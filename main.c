f0dd42417f98cd95fcd0c09d33bbff58180aff32
Erick-D-S <65356418+Erick-D-S@users.noreply.github.com>
2020-05-23 16:07:12 -0500

    added function to free memory, made changes to program flow and output.

--- main.c
+++ main.c
@@ -9,58 +9,76 @@ int main()
 	char firstname[10];
 	char lastname[10];
 	int id = 0;
-	struct customer * first; 
-	struct customer * iterate; 
+	struct customer * first = NULL; 
+	struct customer * iterate = NULL; 
 	while(1)
 	{
-		
 		welcomeScreen();
 		scanf("%d", &decision);
 		
-		if(decision == 1) /*these decisions should be enums. we should also change to a switch*/
+		switch(decision) /*these decisions should be enums.*/
 		{
-			printf("Enter first name \n");
-			scanf("%s", firstname);
-			printf("Enter last name \n");
-			scanf("%s", lastname);
-			createUser(&first, firstname, lastname);
-		}	
-		else if(decision == 2) 
-		{
-			iterate = first;
-			printf("enter your id to log in: \n");
-			scanf("%d", &id);
-			findUser(&iterate, &id);
-			while(id)
+			case 1:
 			{
-				logInScreen();
-				scanf("%d", &decision);
-				
-				switch(decision)
+				printf("Enter first name \n");
+				scanf("%s", firstname);
+				printf("Enter last name \n");
+				scanf("%s", lastname);
+				createUser(&first, firstname, lastname);
+				break;
+			}
+			case 2: 
+			{
+				iterate = first;
+				printf("enter your id to log in: \n");
+				scanf("%d", &id);
+				findUser(&iterate, &id);
+				while(id)
 				{
-					case 1: /*these cases should be enums*/
-						printf("Enter the amount you want to deposit: \n");
-						scanf("%d", &amount);
-						depositMoney(&iterate, amount);
-						break;
-					case 2:
-						printf("Enter the amount you want to withdraw: \n");
-						scanf("%d", &amount);
-						withdrawMoney(&iterate, amount);
-						break;
-					case 3:
-						showBalance(iterate);
-						break;
-					case 4:
-						id = 0;
-						break;
-					default:
-						printf("Enter a valid option\n");
-						break;
-						
+					logInScreen(iterate);
+					scanf("%d", &decision);
+					
+					switch(decision)
+					{
+						case 1: /*these cases should be enums*/
+							printf("\nEnter the amount you want to deposit: \n");
+							scanf("%d", &amount);
+							depositMoney(&iterate, amount);
+							break;
+						case 2:
+							printf("\nEnter the amount you want to withdraw: \n");
+							scanf("%d", &amount);
+							withdrawMoney(&iterate, amount);
+							break;
+						case 3:
+							showBalance(iterate);
+							break;
+						case 4:
+							id = 0;
+							break;
+						default:
+							printf("****\nEnter a valid option****\n\n");
+							break;
+							
+					}
 				}
+				break;
+			}
+			case 3:
+			{
+				deleteUsers(&first);
+				howManyCustomers = 0;
+				break;
+			}
+			case 4:
+			{
+				return 0;
+			}
+			default:
+			{
+				printf("****\nEnter a valid option****\n\n");
+				break;
 			}
 		}
 	}
-	return 0;
 }
--- welcomeScreen.c
+++ welcomeScreen.c
@@ -3,16 +3,18 @@
 
 void welcomeScreen()
 {
-	printf("Welcome to the bank\n");
+	printf("\nWelcome to the bank\n");
 	printf("Choose one of the following options:\n");
 	printf("1. Create new customer\n");
 	printf("2. Log in\n");
+	printf("3. Delete all users\n");
+	printf("4. Shut down\n");
 	return;
 }
 
-void logInScreen()
+void logInScreen(struct customer *customerName)
 {
-	printf("Welcome to the bank\n");
+	printf("\nWelcome %s %s\n", customerName->firstName, customerName->lastName);
 	printf("Choose one of the following options:\n");
 	printf("1. Deposit money\n");
 	printf("2. Withdraw money\n");
